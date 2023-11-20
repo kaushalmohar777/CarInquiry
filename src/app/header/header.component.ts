15bc0d32983eca4d06bf959604c80c1847832135
Kamlesh Mohar <kaushalmohar777@gmail.com>
2023-11-20 16:01:36 +0530

    responsive updation

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { RouteDetailService } from '../route-detail.service';
 
 @Component({
@@ -12,5 +12,10 @@ export class HeaderComponent {
     this._carService.activeClass.subscribe((res:boolean)=>{
       this.activeClass = res
     })
+  }
+
+  @ViewChild('navToggle') navTogglee! :ElementRef; 
+  navTogglebar(){
+    this.navTogglee.nativeElement.classList.remove('show')
   }
 }
